ddffaaafc988053b46eb0bea25b72484242047e2
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-05-04 21:40:26 -0300

    gin-tonic

--- sample/services/deliveries/main.go
+++ sample/services/deliveries/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
